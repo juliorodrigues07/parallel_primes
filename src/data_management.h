c75fa3bec8e07c4f76e38a1a41024ad1c447538b
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-09-22 23:54:25 -0300

    Initial commit

---
+++ src/data_management.h
@@ -0,0 +1,11 @@
+#ifndef __DATA_MANAGEMENT__
+#define __DATA_MANAGEMENT__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <mpi.h>
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "data_management.h"
+
+int main(int argc, char **argv) {
+    printf("Parallel primes\n");
+    return 0;
+}
