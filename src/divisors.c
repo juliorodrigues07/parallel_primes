9b110e5b6dcfb2ed0ea9b7a3cb5a2d5129ac67e5
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-10-19 15:42:53 -0300

    Refact: Changed calculations order greatly improving performance
    Square root of the value and its division by two is now done outside the loop, meaning that this potentially costly operations are now done only one time per value, and not every loop iteration. Also, since matematically an odd number is never divisible  an even number, the loop increment can be increased by two, reducing the number of iterations by half.

--- src/divisors.c
+++ src/divisors.c
@@ -1,24 +1,30 @@
 #include "divisors.h"
 
+#define EVEN 1
+#define ODD 2
+
 int count_divisors (int value) {
 
     if (value == 1)
         return 1;
     else {
         int divisors = 2;
+        int square = sqrt(value);
+        int limit = value / 2;
 
-        for (int i = 2; i <= value / 2; i++) {
+        // Odd numbers aren't divisible by even numbers
+        int step = (value % 2 == 0) ? EVEN : ODD;
 
-            int square = sqrt(value);
+        for (int i = 2; i <= limit; i += step) {
+
             // If checked as far as square root of N, and N doesn't have a divisor greater than 1, then it must be prime
-            if (i == square && divisors == 2)
+            if (i > square && divisors == 2)
                 return 2;
             else {
                 if (value % i == 0)
                     divisors++;
             }
         }
-
         return divisors;
     }
 }
